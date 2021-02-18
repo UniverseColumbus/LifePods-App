2ad91234103f89da73e270339f81b9deb142e4cd
UniverseColumbus <45647736+UniverseColumbus@users.noreply.github.com>
2021-02-18 16:50:05 -0700

    Added ExcelWriter file

--- LifePodsAlgorithm/src/GroupBuilder.java
+++ LifePodsAlgorithm/src/GroupBuilder.java
@@ -3,73 +3,84 @@ import java.util.HashMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GroupBuilder {
-	
-	private static int POD_SIZE = 5;
-	
-	/**
-	 * Method to create the pods
-	 * 
-	 * @param users
-	 * @return pods
-	 */
-	public static ArrayList<LifePod> buildPods(ArrayList<User> users) {
-		// We can write our logic here and return a list of LifePods
-		ArrayList<LifePod> pods = new ArrayList<>();
-		HashMap<Integer, User> usersMapped = new HashMap<>();
-		int maxPods = users.size() / POD_SIZE;
+  
+  private static int POD_SIZE = 5;
+  private ArrayList<User> users;
+  
+  public GroupBuilder(ArrayList<User> users) {
+    this.users = users;
+  }
+  
+  public ArrayList<LifePod> buildPods() {
+    // We can write our logic here and return a list of LifePods
+    ArrayList<LifePod> pods = new ArrayList<>();
+    HashMap<Integer, User> usersMapped = new HashMap<>();
+    int maxPods = users.size() / POD_SIZE;
 
-		// Checks for Users who wants to be leader
-		for (User u : users) {
-			usersMapped.put(u.getId(), u);
-			if (u.getWillingPodLeader().equals("yes") && pods.size() < maxPods) {
-				LifePod newPod = new LifePod();
-				newPod.addUser(u);
-				newPod.addLeader(u);
-				pods.add(newPod);
-			}
-		}
-		
-		// Add users with no preference and anti-preference randomly into groups
-		for (User u : users) {
-			if (u.hasGroup()) continue;
-			boolean noPref = true;
-			for (int id : u.getPreferenceList()) {
-				if (usersMapped.containsKey(id)) noPref = false;
-			}
-			
-			boolean noAntiPref = true;
-			for (int id : u.getAntiPreferenceList()) {
-				if (usersMapped.containsKey(id)) noAntiPref = false;
-			}
-			
-			if (noPref && noAntiPref) {
-				int randomPodIndex = ThreadLocalRandom.current().nextInt(0, pods.size());
-				LifePod podToTry = pods.get(randomPodIndex);
-				while (checkPodAntiPreference(podToTry, u)) {
-					randomPodIndex = ThreadLocalRandom.current().nextInt(0, pods.size());
-					podToTry = pods.get(randomPodIndex);
-					System.out.println("not allowed");
-				}
-				pods.get(randomPodIndex).addUser(u);
-			}
-		}
+    // Checks for Users who wants to be leader
+    for (User u : users) {
+      if (u.getWillingPodLeader().equals("yes") && pods.size() < maxPods) {
+        LifePod newPod = new LifePod();
+        newPod.addUser(u);
+        newPod.addLeader(u);
+        usersMapped.put(u.getId(), u);
+        
+        if (u.getPostGradPlans().equals("work") || u.getPostGradPlans().equals("looking for work")) newPod.setType("work");
+        else if (u.getPostGradPlans().equals("grad school")) newPod.setType("grad school");
+        
+        pods.add(newPod);
+      }
+    }
+    
+    // Add users with no preference and anti-preference randomly into groups
+    for (User u : users) {
+      if (usersMapped.containsKey(u.getId())) continue;
+      
+      boolean havePref = false;
+      for (int id : u.getPreferenceList()) {
+        if (usersMapped.containsKey(id)) havePref = true;
+      }
+      
+      boolean haveAntiPref = false;
+      for (int id : u.getAntiPreferenceList()) {
+        if (usersMapped.containsKey(id)) haveAntiPref = true;
+      }
+      
+      if (havePref == false && haveAntiPref == false) {
+        int randomPodIndex = ThreadLocalRandom.current().nextInt(0, pods.size());
+        LifePod podToTry = pods.get(randomPodIndex);
+        
+        while (welcome(podToTry, u) == false) {
+          randomPodIndex = ThreadLocalRandom.current().nextInt(0, pods.size());
+          podToTry = pods.get(randomPodIndex);
+          System.out.println("not allowed");
+        }
+        pods.get(randomPodIndex).addUser(u);
+      }
+    }
 
-		return pods;
-	}
-	
-	/**
-	 * checks if user is welcome in a certain LifePod
-	 * @param pod
-	 * @param user
-	 * @return true if the user is not welcome
-	 */
-	private static boolean checkPodAntiPreference(LifePod pod, User user) {
-		boolean antiPreference = false;
-		for (User u : pod.getPod()) {
-			for (int id : u.getAntiPreferenceList()) {
-				if (id == user.getId()) return true;
-			}
-		}		
-		return antiPreference;
-	}
+    return pods;
+  }
+  
+  /**
+   * checks if user is welcome in a certain LifePod
+   * @return false if the user is not welcome
+   */
+  private boolean welcome(LifePod pod, User user) {
+    boolean isWelcome = true;
+    for (User u : pod.getMembers()) {
+      for (int id : u.getAntiPreferenceList()) {
+        if (id == user.getId()) isWelcome = false;
+      }
+    }   
+    return isWelcome;
+  }
+  
+  
+
+
+
+
+
+
 }
--- LifePodsAlgorithm/src/Main.java
+++ LifePodsAlgorithm/src/Main.java
@@ -1,24 +1,29 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// file path
-		String file = "src/source/life_pod.csv";
-		ArrayList<User> users;
-		
-		//read file and assign Users to the users ArrayList
-		users = FileReader.readFile(file);		
-		
-		//prints all the users
-		/*for (User u : users) {
-			System.out.println(u.toString());
-		}*/
-		
-		ArrayList<LifePod> pods = GroupBuilder.buildPods(users);
-		for (LifePod p : pods) {
-			System.out.println(p.toString());
-		}
-	}
+  public static void main(String[] args) {
+    // file path
+    String file = "src/source/users.csv";
+    ArrayList<User> users;
+    
+    //read file and assign Users to the users ArrayList
+    users = FileReader.readFile(file);    
+    
+    //prints all the users
+//    for (User u : users) {
+//      System.out.println(u.toString());
+//    }
+//    System.out.println("***********************************");
+    
+    GroupBuilder gb = new GroupBuilder(users);
+    ArrayList<LifePod> pods = gb.buildPods();
+    for (LifePod p : pods) {
+      System.out.println(p.toString());
+    }
+    
+    ExcelWriter ew = new ExcelWriter(pods);
+    ew.write();
+  }
 
 }
