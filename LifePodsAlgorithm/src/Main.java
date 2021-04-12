290bd7a6ec536f63387557d5bc95ef78480b42bd
UniverseColumbus <45647736+UniverseColumbus@users.noreply.github.com>
2021-04-12 00:50:01 -0600

    just switched to gui mode

--- LifePodsAlgorithm/src/Main.java
+++ LifePodsAlgorithm/src/Main.java
@@ -7,11 +7,11 @@ public class Main{
   public static GUI gui;
 
   public static void main(String[] args) {
-//    Main m = new Main();
-//    gui = new GUI(m); 
-//    gui.launch();
+    Main m = new Main();
+    gui = new GUI(m); 
+    gui.launch();
     
-    runWithoutGUI();
+//    runWithoutGUI();
   }
   
   public void finish(String readType, String writeType) {
